b16a1fefd627867bbba972b80cb9a62203f97431
litao <tao.li13@pactera.com>
2019-12-06 18:11:18 +0800

    1206

--- src/main/java/com/molasses/blog/DownloadController.java
+++ src/main/java/com/molasses/blog/DownloadController.java
@@ -1,13 +1,25 @@
 package com.molasses.blog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
 public class DownloadController {
 
     @GetMapping("/download")
-    public String showPage(){
-        return "download page";
+//    @RequestMapping("/download")
+    @ResponseBody
+    public void downloadMolassesInstaller(HttpServletResponse response){
+        System.out.println("in request");
+        String filename = "temp.txt";
+//        System.out.println(filename);
+        String result = FileUtil.downloadFile(response,filename);
+//        System.out.println(result);
     }
 }
